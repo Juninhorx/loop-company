6ec78fba82dd10b34253135e507205e1f5edcc28
Ricardo Xavier <rxc.projects@gmail.com>
2023-11-04 12:34:22 -0300

    alterando responsividade

--- script.js
+++ script.js
@@ -4,9 +4,9 @@ function changeSlide(n) {
     showSlide(slideIndex += n);
 }
 
+const imagemWidth = document.querySelector('.slide').clientWidth
 function showSlide(n) {
     const slides = document.querySelectorAll('.slide');
-
     if (n >= slides.length) {
         slideIndex = 0;
     }
@@ -16,51 +16,8 @@ function showSlide(n) {
     }
 
     slides.forEach(slide => {
-        slide.style.transform = `translateX(-${slideIndex * 756}px)`;
+        slide.style.transform = `translateX(-${slideIndex * imagemWidth}px)`;
     });
 }
 
 showSlide(slideIndex);
-
-
-
-
-
-// const controls = document.querySelectorAll('.control')
-
-// let currentItem  = 0;
-
-// const itens = document.querySelectorAll('.item')
-// const guias = document.querySelectorAll('.guia')
-// const maxItens = itens.length
-
-// controls.forEach(control => {
-//     control.addEventListener('click', () => {
-//         const isLeft = control.classList.contains('arrow-left')
-//         if (isLeft){
-//             currentItem -= 1
-//         } else {
-//             currentItem += 1
-//         }
-
-//         if (currentItem > maxItens - 1) {
-//             currentItem = 0
-//         } else if(currentItem < 0) {
-//             currentItem = maxItens - 1
-//         }
-
-//         guias.forEach(guia => {
-//             guia.classList.remove('current-guia')
-//         })
-//         itens.forEach(item => {
-//             item.classList.remove('current-item')
-//         })
-//         itens[currentItem].scrollIntoView({
-//             inline: "center",
-//             behavior: "smooth"
-//         })
-//         console.log(`currentItem: ${currentItem}`)
-//         itens[currentItem].classList.add('current-item')
-//         guias[currentItem].classList.add('current-guia')
-//     })
-// })
